db8058fcae76e92b6e6cd88b7eb9342a484be397
Algor-Fernand <algorfernand014@gmail.com>
2023-12-02 16:53:52 +0200

    feat:creating methods for users route in the controller

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+     GET/users
+     GET/users/:id
+     POST/users
+     PATCH/users/:id
+     DELETE/users/:id
+     */
+  @Get()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
