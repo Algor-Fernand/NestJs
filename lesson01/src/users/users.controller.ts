eb608d1d42f3d001fde359925d5418ffa1ad6daf
Algor-Fernand <algorfernand014@gmail.com>
2023-12-05 11:56:59 +0200

    feat: add class-validator class-transformer and validationPipe to the controller

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
   ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -20,7 +21,7 @@ export class UsersController {
   findAll(@Query('role') role?: 'intern' | 'engineer' | 'admin') {
     return this.usersService.findAll(role);
   }
-
+  
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
@@ -28,7 +29,7 @@ export class UsersController {
 
   @Post()
   create(
-    @Body()
+    @Body(ValidationPipe)
     createUserDto: CreateUserDto,
   ) {
     return this.usersService.create(createUserDto);
@@ -37,7 +38,7 @@ export class UsersController {
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
-    @Body()
+    @Body(ValidationPipe)
     updateUserDto: UpdateUserDto,
   ) {
     return this.usersService.update(+id, updateUserDto);
